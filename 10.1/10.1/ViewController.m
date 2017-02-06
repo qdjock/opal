6cebd53aa82256e6c17e62f9756aaf7b860de06a
jack <jack@jackdemacbook-pro.local>
2017-02-06 21:57:09 +0800

    2017  2.6\

--- 10.1/10.1/ViewController.m
+++ 10.1/10.1/ViewController.m
@@ -16,6 +16,11 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    UIView *vi = [UIView new];
+    vi.frame =self.view.bounds;
+    vi.backgroundColor =[UIColor purpleColor];
+    [self.view addSubview:vi];
+    
     // Do any additional setup after loading the view, typically from a nib.
 }
 
